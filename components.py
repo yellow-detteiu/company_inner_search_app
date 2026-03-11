d23b3c26576ebaa30497081e56f78bc6feb7dad6
shota_nakano <by2255@cap.ocn.ne.jp>
2026-03-11 13:47:44 +0900

    ページ数の表示内容を修正

--- components.py
+++ components.py
@@ -309,7 +309,7 @@ def display_contact_llm_response(llm_response):
                 # ページ番号を取得
                 page_number = document.metadata["page"]
                 # 「ファイルパス」と「ページ番号」
-                file_info = f"{file_path}（No{page_number+1}）"
+                file_info = f"{file_path}（ページNo{page_number+1}）"
             else:
                 # 「ファイルパス」のみ
                 file_info = f"{file_path}"
